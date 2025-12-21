dad5dbc1cc26d13e7ec4b1363e732452f7f5fb03
David Hill <iamdavidhill@gmail.com>
2025-12-21 16:45:49 +0000

    wip: new release modal
    - highlight key updates or new features
    - needs some transition love
    - all copy including text and video placeholder

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -64,6 +64,7 @@ import { ConstrainDragXAxis } from "@/utils/solid-dnd"
 import { navStart } from "@/utils/perf"
 import { DialogSelectDirectory } from "@/components/dialog-select-directory"
 import { DialogEditProject } from "@/components/dialog-edit-project"
+import { ReleaseNotesHandler } from "@/components/release-notes-handler"
 import { Titlebar } from "@/components/titlebar"
 import { useServer } from "@/context/server"
 
@@ -1182,6 +1183,11 @@ export default function Layout(props: ParentProps) {
   )
 
   createEffect(() => {
+    const sidebarWidth = layout.sidebar.opened() ? layout.sidebar.width() : 48
+    document.documentElement.style.setProperty("--dialog-left-margin", `${sidebarWidth}px`)
+  })
+
+  createEffect(() => {
     const project = currentProject()
     if (!project) return
 
@@ -2146,6 +2152,7 @@ export default function Layout(props: ParentProps) {
         </main>
       </div>
       <Toast.Region />
+      <ReleaseNotesHandler />
     </div>
   )
 }
